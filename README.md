83ab1ab458bd6047bf76d79f586985923abe909f
ceasium <ceasiumzmj@gmail.com>
2024-11-03 00:49:48 +0800

    new class Matrix and ascii, study how to code with lamda sentences and method references and streams and so on

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# ***Hello friends***
+﻿# ***Hello friends***
 Here is Ceasium's java programs written in CUHKSZ  
+
 Of course most of them are simple and junior  
-Because they are my homeworks and trials for some algorithms and functions
-If not nesssery, please don't copy them without any revision, especially for your own homework
-Anyway, if you are inteasted in interested in my programs, you may connect to me by e-mail
+
+Because they are my homeworks and trials for some algorithms and functions  
+
+If not nesssery, please don't copy them without any revision, especially for your own homework  
+
+Anyway, if you are inteasted in interested in my programs, you may connect to me by e-mail  
+
+🤗🥰😴😇🥳
--- Templates/Main.java
+++ Templates/Main.java
@@ -1,33 +1,72 @@
-import java.util.*;
-import java.math.*;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
-    static Scanner input = new Scanner(System.in);
+    public static class Matrix {
+        private final int[][] matrix;
+        private final int rows;
+        private final int columns;
 
-    // Here is the function you need to implement
-    public static int count(boolean[] x) {
-        int counter = 0;
-        for(boolean i:x){
-            if (i){
-                counter++;
+        public Matrix(int[][] matrix) {//int[rows][columns]
+            this.matrix = matrix;
+            this.rows = matrix.length;
+            this.columns = matrix[0].length;
+        }
+
+        public static Matrix Matrixmultiply(Matrix a, Matrix b) {//M a * M b = M c
+            if(a.columns==b.rows){
+                Matrix c = new Matrix(new int[a.rows][b.columns]);
+            int arowcounter = 0;
+            int[] rowab = new int[b.rows];
+            for (int[] rowa : a.matrix) {//for each row in a
+                for (int bcolcounter = 0; bcolcounter < b.columns; bcolcounter++) {//for each column in b
+                    for(int i = 0; i < b.rows; i++){
+                        try{rowab[i] = rowa[i] * b.matrix[i][bcolcounter];}
+                        catch (Exception e){break;}
+                    }
+                    c.matrix[arowcounter][bcolcounter] = Arrays.stream(rowab).sum();
+                }
+                arowcounter++;
+            }
+            return c;
+        }
+            else {
+                System.out.println("a.column!=b.row");
+                return null;
             }
         }
-        return counter-1;
-    }
 
-    public static void main(String[] args){
-        double n = Double.parseDouble(input.nextLine());
-        boolean[] prime = new boolean[(int)n];
-        Arrays.fill(prime, true);
-        for(int i = 2; i <= n; i++) {
-            double gi = Math.sqrt(i);
-            for(int j = 2; j <= gi; j++) {
-                if(i % j == 0) {
-                    prime[i-1] = false;
-                    break;
+        public static int[][] MatrixInput(Scanner input, int rows, int columns) {//input matrix
+            int[][] matrix = new int[rows][columns];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++)
+                    matrix[i][j] = input.nextInt();
+            }
+            return matrix;
+        }
+
+        public static void MatrixOutput(Matrix matrix) {//output matrix
+            try (PrintStream output = System.out) {
+                output.printf("%d %d\n", matrix.rows, matrix.columns);
+                for (int[] row : matrix.matrix) {
+                    for (int i : row)
+                        System.out.print(i + " ");
+                    System.out.println();
                 }
             }
         }
-        System.out.println(count(prime));
+
+        public static void main(String[] args) {
+            Matrix a, b;
+            try (Scanner input = new Scanner(System.in)) {
+                int[] para = Arrays.stream(input.nextLine().split("")).filter(s -> !s.equals(" "))//in
+                        .mapToInt(Integer::parseInt).toArray();//out
+                a = new Matrix(Matrix.MatrixInput(input, para[0], para[1]));
+                b = new Matrix(Matrix.MatrixInput(input, para[1], para[2]));
+            }
+            MatrixOutput(Objects.requireNonNull(Matrixmultiply(a, b)));
+        }
     }
 }
